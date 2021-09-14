74ad4cdea8d48598eb3d53ad2012398617bf26ab
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-09-14 16:36:54 -0300

    finalized

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-$(function(){ /*"menu-btn" é o seletor ou classe */
-   $(".menu-btn").click(()=>{
-       $(".navbar .menu").toggleClass("active");
-       $(".menu-btn i").toggleClass("active");
-   }) 
-})
+$(function () {
+    /*"menu-btn" é o seletor ou classe */ $(".menu-btn").click(() => {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+    $(".navbar .menu li a").click(() => {
+        $("html").css("scrollBehavior", "smooth");
+    });
+});
